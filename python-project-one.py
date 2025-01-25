f91adb1e936e7bda4e212f60b4fba0ead5cb4228
Harper279 <nikolasleftheriotis@gmail.com>
2025-01-25 17:35:03 +0200

    pasted-code

---
+++ python-project-one.py
@@ -0,0 +1,72 @@
+# variables: string, integer, float, boolean
+
+
+#first_name = "bro"
+#food = "pizza"
+#email = "poutsos120@fake.com"
+#color = "red"
+
+# first 4 lines are strings
+
+#print(f"hello {first_name}")
+#print(f"my favourite food is {food}")
+#print(f"my email is {email}")
+#print(f"my favourite color is {color}")
+
+#next lines are integers
+
+#age = 25 
+#quantity = 5
+#student_num = 30
+
+#print(f"you are {age}years old")
+#print(f"i bought {quantity} tomatoes")
+#print(f"my class has {student_num} students")
+
+# next lines are float
+
+#price = 10.99
+#distance = 5.5
+
+#print(f"the price is ${price}")
+#print(f"you ran {distance}km")#
+
+# next lines are boolean
+
+#is_student = True
+#for_sale = True
+#is_online = True
+
+#if is_student:
+#    print("You are a student")
+#else:#
+#    print("You are not a student")
+
+#if for_sale:
+#    print(f"this item is for sale")
+#else:
+#    print(f"this item is NOT for sale")
+
+#if is_online:
+#    print(f"the user is online")
+#else:
+#    print(f"the user is offline")
+
+
+# typecasting: the process of converting a variable from one data type to another
+# for example, when converting an integer to a float it turns from 25 to 25.0(int to float) or in reverse: 5.2 to 5(float to int)
+
+#name = "bro"
+#age = 25
+#gpa = 5.1
+#is_student = True
+
+#print(type(gpa))                #the terminal shows: when print(type(name))  <class 'str'>      because name = "bro" is a string, this happens with the rest of the variables
+
+#age = float(age)               # converting the integer to a float
+
+#print(age)             #output is 25.0
+
+#name = bool(name)          # converting the str to a bool
+
+#print(name)                # output: True#
